f72c0ecb144959a509bb8e5cff17d5c1bacd94e4
daniel-e <git.daniele@gmail.com>
2017-05-10 23:23:59 +0200

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
+//! Crate to generate CAPTCHAs.
+//!
+//! <img src="https://github.com/daniel-e/captcha/raw/master/doc/captcha3.png"> &nbsp;
+//! <img src="https://github.com/daniel-e/captcha/raw/master/doc/captcha2.png">
+//!
+//! The crate offers two ways to create CAPTCHAs. The first way is the most easiest one. Just one
+//! line of code is necessary to create a random CAPTCHA in of a given category. The category can be
+//! either `Easy`, `Medium` or `Hard`. The size of the CAPTCHA is 220x120 pixels and the number of
+//! characters is randomly selected between 4 and 6 letters (inclusive).
+//!
+//! ```
+//! extern crate captcha;
+//! use captcha::samples::{self, Difficulty};
+//!
+//! # fn main() {
+//! let data = samples::gen(Difficulty::Easy).as_png();
+//! # }
+//! ```
+//!
+//! To be more flexible you can build CAPTCHAs manually as well.
+//!
+//! ```
+//! extern crate captcha;
+//! use captcha::Captcha;
+//! use captcha::filters::{Noise, Wave, Dots};
+//!
+//! # fn main() {
+//! let data = Captcha::new()
+//!     .add_chars(5)
+//!     .apply_filter(Noise::new(0.4))
+//!     .apply_filter(Wave::new(2.0, 20.0).horizontal())
+//!     .apply_filter(Wave::new(2.0, 20.0).vertical())
+//!     .view(220, 120)
+//!     .apply_filter(Dots::new(15))
+//!     .as_png();
+//! # }
+//! ```
+
+// TODO rotate characters
+// TODO resize characters
+// TODO overlap characters
+
 extern crate image;
 extern crate rand;
 extern crate serde_json;
@@ -5,6 +47,7 @@ extern crate base64;
 extern crate lodepng;
 
 pub mod filters;
+pub mod samples;
 mod images;
 mod fonts;
 
@@ -17,14 +60,20 @@ use std::io::Result;
 use rand::{thread_rng, Rng};
 use std::cmp::{min, max};
 
+/// Represents the area which contains text in a CAPTCHA.
 #[derive(Clone, Debug)]
 pub struct TextArea {
+    /// The minimum x coordinate of the area which contains text (inclusive).
     pub left: u32,
+    /// The maximum x coordinate of the area which contains text (inclusive).
     pub right: u32,
+    /// The minimum y coordinate of the area which contains text (inclusive).
     pub top: u32,
+    /// The maximum y coordinate of the area which contains text (inclusive).
     pub bottom: u32
 }
 
+/// Used to build a CAPTCHA step by step.
 pub struct Captcha {
     img: Image,
     font: Box<Font>,
@@ -32,7 +81,9 @@ pub struct Captcha {
 }
 
 impl Captcha {
+    /// Returns an empty CAPTCHA.
     pub fn new() -> Captcha {
+        // TODO fixed width + height
         let w = 400;
         let h = 300;
         Captcha {
@@ -44,16 +95,30 @@ impl Captcha {
         }
     }
 
+    /// Applies the filter `f` to the CAPTCHA.
+    ///
+    /// This method is used to add noise, grids, etc or to transform the shape of the CAPTCHA.
     pub fn apply_filter<F: Filter>(&mut self, f: F) -> &mut Self {
         f.apply(&mut self.img);
         self
+        // TODO support other fonts
     }
 
+    /// Sets another font that is used for the characters.
+    ///
+    /// Calling this method does not have an effect on the font of the characters which have already
+    /// been added to the CAPTCHA. The new font is only applied to the characters which are written
+    /// to the CAPTCHA after this method is called.
     pub fn set_font<F: Font + 'static>(&mut self, f: F) -> &mut Self {
         self.font = Box::new(f);
         self
+        // TODO support other fonts
     }
 
+    /// Saves the CAPTCHA to a image file.
+    ///
+    /// The format that is written is determined from the filename's extension. On error `Err` is
+    /// returned.
     pub fn save(&self, p: &Path) -> Result<()> { self.img.save(p) }
 
     fn random_char_as_image(&self) -> Option<Image> {
@@ -69,6 +134,7 @@ impl Captcha {
         }
     }
 
+    /// Adds a random character using the current font.
     pub fn add_char(&mut self) -> &mut Self {
         match self.random_char_as_image() {
             Some(i) => {
@@ -84,8 +150,10 @@ impl Captcha {
         }
 
         self
+        // TODO automatically resize if many characters are added
     }
 
+    /// Adds a red box to the CAPTCHA representing the area which contains text.
     pub fn add_text_area(&mut self) -> &mut Self {
         for y in self.text_area.top..self.text_area.bottom {
             self.img.put_pixel(self.text_area.left, y, Pixl::red());
@@ -98,11 +166,15 @@ impl Captcha {
         self
     }
 
+    /// Returns the geometry of the area which contains text in the CAPTCHA.
     pub fn text_area(&self) -> TextArea {
         self.text_area.clone()
     }
 
+    /// Crops the CAPTCHA to the given geometry.
     pub fn extract(&mut self, area: TextArea) -> &mut Self {
+        // TODO rename the method
+        // TODO adjust the text area
         let w = area.right - area.left + 1;
         let h = area.bottom - area.top + 1;
         let mut i = Image::new(w, h);
@@ -115,6 +187,8 @@ impl Captcha {
         self
     }
 
+    /// Crops the CAPTCHA to the given width and height with the text centered withing this
+    /// box.
     pub fn view(&mut self, w: u32, h: u32) -> &mut Self {
         let mut a = self.text_area();
         a.left   = (a.right + a.left) / 2 - w / 2;
@@ -123,9 +197,11 @@ impl Captcha {
         a.bottom = a.top + h;
         self.extract(a);
         // TODO update text area
+        // TODO what happens if w or h are too small
         self
     }
 
+    /// Adds the given number of random characters to the CAPTCHA using the current font.
     pub fn add_chars(&mut self, n: u32) -> &mut Self {
         for _ in 0..n {
             self.add_char();
@@ -133,7 +209,10 @@ impl Captcha {
         self
     }
 
-    pub fn as_png(self) -> Option<Vec<u8>> {
+    /// Returns the CAPTCHA as a png image.
+    ///
+    /// Returns `None` on error.
+    pub fn as_png(&self) -> Option<Vec<u8>> {
         self.img.as_png()
     }
 }
@@ -141,7 +220,7 @@ impl Captcha {
 #[cfg(test)]
 mod tests {
     use Captcha;
-    use filters::{Noise, Grid, Cow, Dots, Wave, Direction};
+    use filters::{Noise, Grid};
     use fonts::Default;
 
     use std::path::Path;
