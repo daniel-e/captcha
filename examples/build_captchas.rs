8e26d427eb87f96f188e75b46b93869620e86837
daniel-e <git.daniele@gmail.com>
2020-10-31 19:06:18 +0100

    example how to get the CAPTCHA text

--- examples/build_captchas.rs
+++ examples/build_captchas.rs
@@ -6,8 +6,8 @@ use captcha::{Captcha, Geometry};
 use std::path::Path;
 
 fn main() {
-    Captcha::new()
-        .add_chars(5)
+    let mut c = Captcha::new();
+    c.add_chars(5)
         .apply_filter(Noise::new(0.2))
         .apply_filter(Wave::new(2.0, 20.0))
         .view(220, 120)
@@ -17,8 +17,9 @@ fn main() {
                 .max_radius(50)
                 .circles(1)
                 .area(Geometry::new(40, 150, 50, 70)),
-        )
-        //        .apply_filter(Cow::new().min_radius(20).max_radius(30).circles(1).area(Geometry::new(50, 80, 30, 80)))
-        .save(Path::new("captcha.png"))
+        );
+    c.save(Path::new("captcha.png"))
         .expect("save failed");
+
+    println!("CAPTCHA with text {} written to captcha.png", c.chars_as_string());
 }
